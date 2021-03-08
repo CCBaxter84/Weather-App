f2739582bb22b9c3421cbc554a199f319b848c2d
CCBaxter84 <troyceclark@gmail.com>
2021-03-08 12:10:22 -0700

    Set up express server and db

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app == express();
+require('dotenv').config();
+
+// Initialize app
+const app = express();
 
 // Use body parsing
 app.use(express.json());
+
+// Connect to database
+const db = process.env.mongoURI;
+const settings = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+mongoose
+  .connect(db, settings)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
